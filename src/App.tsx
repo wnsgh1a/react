c452006080d3d18cd1f9a8ef1478a547630abade
최준호 <wnsgh1a@naver.com>
2025-07-10 14:07:22 +0900

    2025-07-10 new commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function App() {
+  // userState => Hooks
+  // userState는 가장 기본적인 Hook이며, 함수 컴포넌트에서도 가변적인 상태를 지닐 수 있게 해준다
+  // 이 함수가 호출되면 배열을 반환한다. => 첫번째 요소는 상태값, 두번쨰 요소는 상태를 설정하는 함수
+  // userState 함수의 파라미터에는 상태의 기본값을 넣어줍니다
+
+  const [value, setValue] = useState(0);
+
+  const increment = () => setValue(value + 1);
+  const decrement = () => setValue(value - 1);
+
+  return (
+    <div>
+      <p>
+        현재 카운터값은 <b>{value}</b>
+      </p>
+      <button onClick={increment}>1 증가</button>
+      <button onClick={decrement}>1 감소</button>
+    </div>
+  );
+}
+
+export default App;
